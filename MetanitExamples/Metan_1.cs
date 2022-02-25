85bf3bccc96d50133ade9385f346d44cbb42b1c9
Abdullaev-kosimzho-002 <dzukosta348@gmail.com>
2022-02-25 22:08:05 +0300

    Добавьте файлы проекта.

---
+++ MetanitExamples/Metan_1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetanitExamples
+{
+    class Metan_1
+    {
+        //Task 1.
+
+        /*public string name = "Sam";
+        public int age;
+
+        public Metan_1(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+
+        }*/
+
+        //Task 2.
+        /*
+        public string name = "Ben";
+        public int age = 18;
+        public string email = "ben@gmail.com";
+
+        public Metan_1(string name)
+        {
+            this.name = name;
+        }
+        public Metan_1(string name, int age) : this(name)
+        {
+            this.age = age;
+
+        }
+        public Metan_1(string name, int age, string email) : this("Bob", age)
+        {
+            this.email = email;
+        }
+             
+        */
+
+        //Question 5.
+
+        /* int age = 26;  //По умолчанию указана private
+          string name = "Tom";
+
+          public Metan_1(int age, string name)
+          {
+              this.age = age;
+              this.name = name;
+
+          }
+        */
+
+
+        //Number 4.
+        /*
+        private int age = 15;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+
+            }
+        }
+
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MetanitExamples/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace MetanitExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Number 1.
+
+            /*Metan_1 tom = new Metan_1("Tom", 34)
+            {
+                name = "Bobe",
+                age = 29
+            };
+            Console.WriteLine(tom.name);
+            */
+
+
+            //Number 2.
+            /*
+            Metan_1 person = new Metan_1("Tom", 31, "tom@gmail.com");
+            Console.WriteLine($"{person.name}, {person.email}, {person.age}");
+            */
+
+
+            //Question 5.
+            /*
+            Metan_1 person = new Metan_1(19, "Bob");
+            Console.WriteLine(person);
+            */
+
+            //Number 4.
+            /*
+            Metan_1 person = new Metan_1();
+            person.Age = 25;
+            Console.WriteLine(person.Age);
+              */   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
